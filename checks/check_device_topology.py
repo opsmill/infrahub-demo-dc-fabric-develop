3452d997ca6b167c5b59663eb2a0a7e7cb782ec3
Benoit Kohler <BeArchiTek@users.noreply.github.com>
2024-02-21 13:56:44 +0100

    Wvd 20240220 user defined check (#1)
    * implement user defined check check_device_topology
    
    * temporary disable everything but checks in .infrahub.yml
    
    ---------
    
    Co-authored-by: Wim Van Deun <wim@mbpwimvandeun.tailc018d.ts.net>

--- checks/check_device_topology.py
+++ checks/check_device_topology.py
@@ -1,38 +1,39 @@
-from collections import defaultdict
+from collections import Counter, defaultdict
 
-from infrahub.checks import InfrahubCheck
+from infrahub_sdk.checks import InfrahubCheck
 
 
 class InfrahubCheckDeviceTopology(InfrahubCheck):
 
     query = "check_device_topology"
 
-    def validate(self):
+    def validate(self, data):
 
-        device_roles = {}
-        expected_roles = {}
+        site_device_roles = defaultdict(list)
+        site_expected_device_roles = defaultdict(dict) 
 
-        for device in self.data["data"]["InfraDevice"]["edges"]:
+        for device in data["data"]["InfraDevice"]["edges"]:
             role = device["node"]["role"]["value"]
             site = device["node"]["site"]["node"]["name"]["value"]
+            site_device_roles[site].append(role)
 
-            if site not in device_roles:
-                device_roles[site] = {}
+        for site in site_device_roles.keys(): 
+            site_device_roles[site] = Counter(site_device_roles[site])
 
-            if role in device_roles:
-                device_roles[site][role] += 1
-            else:
-                device_roles[site][role] = 1
-
-        for element in self.data["data"]["InfraTopologyElement"]["edges"]:
-            expected_role = element["node"]["name"]["value"]
-            expected_roles[expected_role] = element["node"]["amount"]["value"]
-
-        for site in device_roles.items():
-            for role in device_roles[site].items():
-                if device_roles[site][role] != expected_roles[role]:
+        for topology in data["data"]["TopologyTopology"]["edges"]:
+            if not topology["node"]["location"]["node"]:
+                continue
+            site = topology["node"]["location"]["node"]["name"]["value"]
+            for element in topology["node"]["elements"]["edges"]:
+                role = element["node"]["device_role"]["value"]
+                quantity = element["node"]["quantity"]["value"]
+                site_expect_device_roles[site][role] = quantity
+                
+        for site, roles in site_expected_device_roles.items():
+            for role, quantity in roles.items():
+                if quantity != site_device_roles.get(site, {}).get(role, 0):
                     self.log_error(
-                        message=f"{site} does not have expected amount of {role} ({device_roles[site][role]}/{expected_roles[role]})",
+                        message=f"{site} does not have expected amount of devices with role {role}",
                         object_id=role,
                         object_type="role",
                     )
