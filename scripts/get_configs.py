37a41572fe2325733cc2c3dba9b694ddab369d74
Marco Martinez <mmmartinez@bluewin.ch>
2024-05-24 12:03:47 +0200

    rewrite it to be sync

--- scripts/get_configs.py
+++ scripts/get_configs.py
@@ -1,50 +1,39 @@
 import requests, os
-from infrahub_sdk import InfrahubClient
+from infrahub_sdk import InfrahubClientSync
 import asyncio
 
 
 
-async def getContainerlabTopology():
+def get_containerlab_topology():
     directory_path = "./generated-configs/clab"
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
-    client = InfrahubClient()
-    topologies = await client.all(kind="TopologyTopology")
+    client = InfrahubClientSync.init()
+    topologies = client.all(kind="TopologyTopology")
 
     for topology in topologies:
-        await topology.artifacts.fetch()
-        for artifact in topology.artifacts.peers:
-            if (artifact.display_label == "Containerlab Topology"):
-                artifact = downloadArtifact(artifact.peer.storage_id.value)
-                with open(f'{directory_path}/{topology.name.value}.yml', 'w') as file:
-                    file.write(artifact.decode('utf-8'))
+        artifact = topology.artifact_fetch("Containerlab Topology")
+        with open(f'{directory_path}/{topology.name.value}.yml', 'w') as file:
+            file.write(artifact)
 
-async def getDeviceConfigs():
+def get_device_configs():
     directory_path = "./generated-configs/clab/configs/startup"
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
-    client = InfrahubClient()
-    devices = await client.all(kind="InfraDevice")
+    client = InfrahubClientSync.init()
+    devices = client.all(kind="InfraDevice")
 
     for device in devices:
-        await device.artifacts.fetch()
+        device.artifacts.fetch()
         for artifact in device.artifacts.peers:
             if (str(artifact.display_label).startswith("Startup Config")):
-                artifact = downloadArtifact(artifact.peer.storage_id.value)
+                artifact = device.artifact_fetch(artifact.display_label)
                 with open(f'{directory_path}/{device.name.value}.cfg', 'w') as file:
-                    file.write(artifact.decode('utf-8'))
-
-def downloadArtifact(artifact_id):
-    response = requests.get(f"http://localhost:8000/api/storage/object/{artifact_id}")
-
-    if response.status_code == 200:
-        return(response.content)
-    else:
-        print(f"Failed to download file. Status code: {response.status_code}")
+                    file.write(artifact)
 
 
 
-asyncio.run(getContainerlabTopology())
-asyncio.run(getDeviceConfigs())
+get_containerlab_topology()
+get_device_configs()
